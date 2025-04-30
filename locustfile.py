cc9b1771feeac80a6ff642c913ae38a610737150
Souvik Sarkar <souviksarkar2k3@gmail.com>
2025-04-30 13:32:36 +0530

    Locust stress testing script

---
+++ locustfile.py
@@ -0,0 +1,12 @@
+from locust import HttpUser, task, between
+import random
+
+
+class RateLimiterUser(HttpUser):
+    wait_time = between(0.001, 0.005)
+
+    @task
+    def protected_endpoint(self) -> None:
+        user_id = str(random.randint(1, 10_000))
+        headers = {'X-User-Id': user_id}
+        self.client.get('/protected', headers=headers)
